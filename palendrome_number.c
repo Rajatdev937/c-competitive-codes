f069603eec14c208a3e96a5e0c3834a5ef72c649
Rajat Negi <007negirajat@gmail.com>
2025-12-23 12:00:57 +0530

    new problem

---
+++ palendrome_number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main ()
+{
+int n;
+printf("enter the number:");
+scanf("%d",&n);
+int original=n;
+int reverse=0;
+int remainder;
+
+while(n!=0){
+remainder=n%10;
+reverse=reverse*10+remainder;
+n=n/10;
+}
+if(original==reverse)
+    printf("its a palindrome number\n");
+else
+printf("its not a palindrome\n");
+return 0;
+}
