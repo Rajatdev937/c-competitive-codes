5de8af969d01e44ff997565e6dd41a5dae61da82
Rajat Negi <007negirajat@gmail.com>
2026-01-09 23:29:27 +0530

    new problem

---
+++ reverse_string2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "abcdefg";
+    int size = strlen(str);
+    int i, k = 2;
+
+    for (i = 0; i < size; i += 2 * k) {
+        int left = i;
+        int right = i + k - 1;
+
+        if (right >= size)
+            right = size - 1;
+
+        while (left < right) {
+            char temp = str[left];
+            str[left] = str[right];
+            str[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    printf("%s", str);
+    return 0;
+}
